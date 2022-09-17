3669de92e7ca3a27218e9a6bef7cabd29644c4ac
xhayper <hayper1919@gmail.com>
2022-09-17 19:28:22 +0700

    feat: OwO

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import ReactDOM from "react-dom/client";
+import theme from "./theme";
 import React from "react";
 import App from "./App";
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <ChakraProvider theme={theme}>
+            <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+            <App />
+        </ChakraProvider>
     </React.StrictMode>
 );
