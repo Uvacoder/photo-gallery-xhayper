fbb477a6d28815fc47d197f4fed70020a8840f5c
xhayper <hayper1919@gmail.com>
2022-09-20 15:14:38 +0700

    feat: forgot

---
+++ app.vue
@@ -0,0 +1,32 @@
+<script setup lang="tsx">
+useHead({
+    title: "hayper",
+    link: [
+        { rel: "icon", type: "image/png", href: "/favicon.png" }
+    ],
+    meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1, shrink-to-fit=no" },
+        { name: "theme-color", content: "#57739A" },
+        // OpenGraph
+        { property: "og:type", content: "website" },
+        { property: "og:title", content: "hayper" },
+        { property: "og:url", content: "https://portfolio-xhayper.vercel.app" },
+        { property: "og:image", content: "https://portfolio-xhayper.vercel.app/gallery/hayper/WOOOO.png" },
+        { property: "og:description", content: "Furry | Backend developer" },
+        // Twitter
+        { name: "twitter:card", content: "summary_large_image" },
+        { name: "twitter:title", content: "hayper" },
+        { name: "twitter:site", content: "@hayper1919" },
+        { name: "twitter:description", content: "Furry | Backend developer" },
+        { name: "twitter:image", content: "https://portfolio-xhayper.vercel.app/gallery/hayper/WOOOO.png" }
+    ]
+})
+</script>
+
+<template>
+    <div>
+        <NuxtLayout>
+            <NuxtPage />
+        </NuxtLayout>
+    </div>
+</template>
---
+++ components/navbar.vue
@@ -0,0 +1,39 @@
+<script setup lang="tsx">
+import { useDark, useToggle } from '@vueuse/core';
+
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+
+const router = useRouter();
+</script>
+
+<template>
+    <b-navbar toggleable="lg" dark sticky="top" style="background-color: #57739A;">
+        <b-navbar-brand to="/">
+            <b-img src="/favicon.png" width="48" />
+        </b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav class="me-auto">
+                <b-nav-item to="/about" :disabled="!router.hasRoute('about')">About</b-nav-item>
+                <b-nav-item to="/contact" :disabled="!router.hasRoute('contact')">Contact</b-nav-item>
+                <b-nav-item to="/gallery" :disabled="!router.hasRoute('gallery')">Gallery</b-nav-item>
+            </b-navbar-nav>
+            <b-navbar-nav>
+                <b-nav-item href="https://github.com/xhayper/portfolio" target="_blank" rel="noopener">
+                    <b-button class='bi bi-github' :variant="isDark ? 'secondary' : 'light'" />
+                </b-nav-item>
+                <b-nav-item>
+                    <b-button @click="toggleDark()" :variant="isDark ? 'secondary' : 'light'">
+                        <i :class="{
+                            'bi bi-moon-fill': isDark,
+                            'bi bi-sun': !isDark
+                        }" />
+                    </b-button>
+                </b-nav-item>
+            </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
+</template>
