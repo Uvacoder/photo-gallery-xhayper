0d61923def84952e1bc42b5775773aaedb841aed
xhayper <hayper1919@gmail.com>
2022-09-20 15:46:26 +0700

    feat: do stuff the correct way

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    ssr: false,
     modules: ["bootstrap-vue-3/nuxt", "@vueuse/nuxt"],
     css: ["@/assets/css/global.scss"],
+    sourcemap: {
+        client: true
+    },
     vite: {
         build: {
             sourcemap: true
