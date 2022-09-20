91afb4cc702e34992155a5e4b94a877de039caf4
xhayper <hayper1919@gmail.com>
2022-09-20 16:59:37 +0700

    feat: screw it

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,12 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     ssr: false,
+    target: 'static',
     modules: ["bootstrap-vue-3/nuxt", "@vueuse/nuxt"],
     css: ["@/assets/css/global.scss"],
     sourcemap: {
-        client: true
+        client: true,
+        server: true
     },
     vite: {
         build: {
