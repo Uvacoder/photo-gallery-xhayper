57f5fd20cf5943f0c9c1c668dfadce279e2316f3
xhayper <hayper1919@gmail.com>
2022-09-20 15:31:39 +0700

    feat: nvm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    ssr: false,
     modules: ["bootstrap-vue-3/nuxt", "@vueuse/nuxt"],
     css: ["@/assets/css/global.scss"],
     vite: {
