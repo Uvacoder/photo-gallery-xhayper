5b637f41b2277de2cdb445ecd0ec8d879bf0fbac
xhayper <hayper1919@gmail.com>
2022-11-04 10:11:24 +0700

    feat: fix navbar color

--- components/navbar.vue
+++ components/navbar.vue
@@ -9,11 +9,11 @@
               class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
               <span class="sr-only">Open main menu</span>
               <svg v-if="!open" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="block w-6 h-6" aria-hidden="true">
+                stroke="currentColor" class="block w-6 h-6 bg-gray-700" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
               </svg>
               <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="block w-6 h-6" aria-hidden="true">
+                stroke="currentColor" class="block w-6 h-6 bg-gray-700" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
               </svg>
 
